6ab5941a106dd0e98d0ca87288daf972b1382eae
akshat0047 <pande.akshat21@gmail.com>
2020-07-28 02:11:22 +0530

    added wow.js, typed.js, removed vanta.js

---
+++ revamped/vastu/new/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+//Typed
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    loop: true,
+    loopCount: Infinity,
+    showCursor: false,
+    typeSpeed: 50
+  });
+    
+});
