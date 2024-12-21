cfd983c9ef93b77aa6a85bfd700fee3181e14373
akshatgsg0047 <akshat.pande@joingsg.com>
2024-12-21 22:05:55 +0530

    fix: added new build

--- projects/src/App.js
+++ projects/src/App.js
@@ -6,37 +6,46 @@ const projectImages = require.context('./assets/img', false, /\.(png|jpe?g|svg)$
 
 const projects = [
   {
-    title: "Blog IT",
-    description: "Blogging website on Django",
-    tech: ["python", "django", "jinja2"],
-    thumbnail: projectImages("./djangoBlog.png"),
-    video: null,
-    liveDemo: "https://akshat0047.pythonanywhere.com/",
-    sourceCode: "https://github.com/akshat0047/first-django-blog",
-  },
-  {
     title: "Video Editor",
     description: "Video Editing APIs",
     tech: ["nodejs", "ffmpeg", "swagger"],
-    thumbnail: projectImages("./video-editor.png"),
+    thumbnail: projectImages("./video.png"),
     video: null,
     liveDemo: "https://videoeditor-production.up.railway.app/api-docs",
     sourceCode: "https://github.com/akshat0047/VideoEditor",
   },
   {
+    title: "Blog IT",
+    description: "Blogging website on Django",
+    tech: ["python", "django", "jinja2"],
+    thumbnail: projectImages("./blog.jpeg"),
+    video: null,
+    liveDemo: "https://akshat0047.pythonanywhere.com/",
+    sourceCode: "https://github.com/akshat0047/first-django-blog",
+  },
+  {
     title: "Socket Chat",
     description: "Chatrooms based on socket.io",
     tech: ["nodejs", "socket.io", "react"],
-    thumbnail: projectImages("./socket-chat.png"),
+    thumbnail: projectImages("./chat.png"),
     video: null,
     liveDemo: "https://socket-chat-vr6j.onrender.com/",
     sourceCode: "https://github.com/akshat0047/first-django-blog",
   },
+  // {
+  //   title: "Yaksh Kubernetes",
+  //   description: "POC for orchestration of Yaksh (open-source)",
+  //   tech: ["python", "flask", "jinja2"],
+  //   thumbnail: projectImages("./kubernetes.png"),
+  //   video: null,
+  //   liveDemo: null,
+  //   sourceCode: "https://github.com/akshat0047/yaksh-devops",
+  // },
   {
     title: "AmiXchange",
     description: "A website to re-sell books",
     tech:"php,jquery,html,css",
-    thumbnail: projectImages("./amixchange.png"),
+    thumbnail: projectImages("./book.jpeg"),
     video: "https://drive.google.com/file/d/1Pd4_E2V-1ZDDYJHXKw9yVmxUk-oqLDvd/view?usp=drive_link",
     liveDemo: null,
     sourceCode: "https://github.com/akshat0047/AmiXchange",
@@ -45,7 +54,7 @@ const projects = [
     title: "Phas-Gaming",
     description: "Game based on phasor.js framework",
     tech: ["JavaScript", "Phaser", "html", "css"],
-    thumbnail: projectImages("./gaming.png"),
+    thumbnail: projectImages("./phasor.jpeg"),
     video: null,
     liveDemo: "https://akshat0047.github.io/phasgming/Mock-Game/",
     sourceCode: "https://github.com/akshat0047/phasgming",
@@ -54,38 +63,11 @@ const projects = [
     title: "AWDash",
     description: "Analytics dashboard for FOSSEE websites.",
     tech: ["php", "bootstrap", "html", "css"],
-    thumbnail: projectImages("./awdash.png"),
+    thumbnail: projectImages("./analytics.jpeg"),
     video: null,
     liveDemo: "https://awdash.fossee.in/index.html",
     sourceCode: "https://github.com/FOSSEE/AWDash",
-  },
-  {
-    title: "Yaksh Kubernetes",
-    description: "POC for orchestration of Yaksh (open-source)",
-    tech: ["python", "flask", "jinja2"],
-    thumbnail: projectImages("./kubernetes.png"),
-    video: null,
-    liveDemo: null,
-    sourceCode: "https://github.com/akshat0047/yaksh-devops",
-  },
-  {
-    title: "Telegram Chatbot",
-    description: "Improvised MapMyIndia API for telegram",
-    tech: ["javascript"],
-    thumbnail: projectImages("./telegram-bot.png"),
-    video: "https://drive.google.com/file/d/1KNv4C-ynJXk5ELQ3nCIyBFPIl-52ylDd/view?usp=sharing",
-    liveDemo: null,
-    sourceCode: "https://github.com/akshat0047/mmi-telegram-bot",
-  },
-  {
-    title: "Fraped",
-    description: "Small website scraping web-app",
-    tech: ["python", "flask", "jinja2"],
-    thumbnail: projectImages("./fraped.png"),
-    video: "https://drive.google.com/file/d/13JVIt8p5yMoY2zLhi7O43PGutwHeqXbq/view?usp=drive_link",
-    liveDemo: null,
-    sourceCode: "https://github.com/akshat0047/Fraped",
-  },
+  }
   // Add more projects as needed
 ];
 
@@ -94,7 +76,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Akshat's Projects</h1>
-        <p>Solutions that I've built personally, at internships and in fulltime roles</p>
       </header>
       <main className="project-grid">
         {projects.map((project, index) => (
